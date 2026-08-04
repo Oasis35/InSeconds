3e88612a813511bfc4f7d181292a3981730c7b1b
crageau <clement.rageau@gmail.com>
2026-08-04 20:38:39 +0200

    commit

--- src/front/InSeconds.Client/src/app/shared/deezer-badge.component.ts
+++ src/front/InSeconds.Client/src/app/shared/deezer-badge.component.ts
@@ -1,4 +1,5 @@
-import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, computed, inject, input, ChangeDetectionStrategy } from '@angular/core';
+import { LanguageService } from '../core/services/language.service';
 
 @Component({
   selector: 'app-deezer-badge',
@@ -6,6 +7,15 @@ import { Component, input, ChangeDetectionStrategy } from '@angular/core';
   templateUrl: './deezer-badge.component.html',
 })
 export class DeezerBadgeComponent {
+  private readonly language = inject(LanguageService);
+
+  readonly deezerTrackId = input<number | undefined>();
+  readonly variant = input<'horizontal' | 'vertical'>('horizontal');
   readonly width = input('196');
   readonly height = input('28');
+
+  protected readonly href = computed(() =>
+    this.deezerTrackId() != null ? `https://www.deezer.com/track/${this.deezerTrackId()}` : null,
+  );
+  protected readonly isEn = computed(() => this.language.current() === 'en');
 }
